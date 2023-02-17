84786603856a427b905556450fef913221fe3b63
Nariman <abonent26pg@mail.ru>
2023-02-17 23:28:50 +0300

    added redux connect

--- counter-react/src/components/Counter.jsx
+++ counter-react/src/components/Counter.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-
+import { connect } from "react-redux";
+import * as actions from "../actions";
 const Counter = ({ counter, inc, dec, rnd }) => {
   return (
     <div className="jumbotron">
@@ -17,4 +17,14 @@ const Counter = ({ counter, inc, dec, rnd }) => {
   );
 };
 
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    counter: state.value,
+  };
+};
+
+// const mapDispatchToProps = (dispatch) => {
+//   return bindActionCreators(actions, dispatch);
+// };
+
+export default connect(mapStateToProps, actions)(Counter);
